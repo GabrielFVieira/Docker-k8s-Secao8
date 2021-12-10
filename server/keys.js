8cc2f8c3d09bb958dd965e9ec8886fc1e17c4965
Gabriel Figueiredo Vieira <gabriel.vieira@sensedia.com>
2021-12-10 18:09:15 -0300

    first commit

---
+++ server/keys.js
@@ -0,0 +1,9 @@
+module.exports = {
+    redisHost: process.env.REDIS_HOST,
+    redisPort: process.env.REDIS_PORT,
+    pgUser: process.env.PG_USER,
+    pgHost: process.env.PG_HOST,
+    pgPort: process.env.PG_PORT,
+    pgDB: process.env.PG_DB,
+    pgPwd: process.env.PG_PWD
+};
